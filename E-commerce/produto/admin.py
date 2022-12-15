f67a8c37d73b91bbde29aebb8f28ae281157a047
marcelokfws <30984850+marcelokfws@users.noreply.github.com>
2022-12-14 22:15:52 -0300

    .

--- E-commerce/produto/admin.py
+++ E-commerce/produto/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from produto.models import Produto
+from . import models
 
-admin.site.register(Produto)
+
+class VariacaoInline(admin.TabularInline):
+    model = models.Variacao
+    
+    
+    extra = 1
+    
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+   
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(models.Produto, ProdutoAdmin)
+admin.site.register(models.Variacao)
