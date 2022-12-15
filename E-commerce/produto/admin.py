04114ba850b2cdaf0d72a02c5a117da36043ccee
marcelokfws <30984850+marcelokfws@users.noreply.github.com>
2022-12-14 21:41:23 -0300

    .

--- E-commerce/produto/admin.py
+++ E-commerce/produto/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from produto.models import Produto
+
+admin.site.register(Produto)
--- E-commerce/produto/models.py
+++ E-commerce/produto/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
+import os
+from PIL import Image
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(
+        default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+    def __str__(self):
+        return self.nome
