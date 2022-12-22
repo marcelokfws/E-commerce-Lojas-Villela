6980482e227f34ad7741ec004387400a68eb7311
marcelokfws <30984850+marcelokfws@users.noreply.github.com>
2022-12-22 16:42:26 -0300

    .

--- E-commerce_02/pedido/views.py
+++ E-commerce_02/pedido/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
-
-class Pagar(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Pagar')
+from django.contrib import messages
 
 class SalvarPedido(View):
+    template_name = 'pedido/pagar.html'
+
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Você precisa fazer login.'
+            )
+            return redirect('perfil:criar')
+        
+        contexto = {
+            
+        }
+
+        return render(self.request,self.template_name, contexto)
+
+class Pagar(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Fechar Pedido')
 
