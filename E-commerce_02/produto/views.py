c0206e8d902332a9b4c49c7951524f54d8c9ea44
marcelokfws <30984850+marcelokfws@users.noreply.github.com>
2022-12-22 03:08:33 -0300

    .

--- E-commerce_02/produto/views.py
+++ E-commerce_02/produto/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
+from django.db.models import Q
+
 from . import models
-from pprint import pprint
+from perfil.models import Perfil
 
 class ListaProduto(ListView):
     model = models.Produto
@@ -147,4 +149,29 @@ class Carrinho(View):
         return render(self.request, 'produto/carrinho.html', contexto)
 
 class ResumoDaCompra(View):
-    pass
+     def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+
+        perfil = Perfil.objects.filter(usuario=self.request.user).exists()
+
+        if not perfil:
+            messages.error(
+                self.request,
+                'Usuário sem perfil.'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio.'
+            )
+            return redirect('produto:lista')
+
+        contexto = {
+            'usuario': self.request.user,
+            'carrinho': self.request.session['carrinho'],
+        }
+
+        return render(self.request, 'produto/resumodacompra.html', contexto)
